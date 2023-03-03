5d4fc7ffd3f4c68a99ccc159d8d775533a519a61
eshhusv <ak7527203@gmail.com>
2023-03-03 18:43:33 +0200

    Add project files.

---
+++ onedotone/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите а: ");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Введите b: ");
+double b = double.Parse(Console.ReadLine());
+Console.Write("Введите c: ");
+double c = double.Parse(Console.ReadLine());
+Console.WriteLine($"Ответ: {((b + Math.Sqrt(Math.Pow(b, 2) + 4 * a * c))/(2 * a)) - Math.Pow(a, 3) * c + Math.Pow(b, -2)}");
